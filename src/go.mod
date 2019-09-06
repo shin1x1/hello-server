a2b6d31102f03c80c8d91e0c10c10f29874483ad
shin1x1 <shin1x1@gmail.com>
2019-09-06 16:46:11 +0900

    add dbHandler and redisHandler

--- src/go.mod
+++ src/go.mod
@@ -4,10 +4,15 @@ go 1.12
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/go-redis/redis v6.15.5+incompatible
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.2.8 // indirect
+	github.com/lib/pq v1.2.0
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/onsi/ginkgo v1.10.1 // indirect
+	github.com/onsi/gomega v1.7.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/valyala/fasttemplate v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20190411191339-88737f569e3a // indirect
 )
--- src/hello.go
+++ src/hello.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"os"
 
+	"github.com/go-redis/redis"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -19,6 +22,8 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "ok")
 	})
+	e.GET("/health_db", dbHandler)
+	e.GET("/health_redis", redisHandler)
 	e.GET("/env", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{
 			"SECRET_VALUE": os.Getenv("SECRET_VALUE"),
@@ -26,4 +31,35 @@ func main() {
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func dbHandler(c echo.Context) error {
+	var db *sql.DB
+	var err error
+
+	db, err = sql.Open("postgres", os.Getenv("DB_DSN"))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	var version string
+	err = db.QueryRow("SELECT version()").Scan(&version)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.String(http.StatusOK, version)
+}
+
+func redisHandler(c echo.Context) error {
+	client := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_HOST"),
+	})
+
+	result, err := client.Info("server").Result()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.String(http.StatusOK, result)
 }
